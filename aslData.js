72526a87807064d539fadc0424cb397325202b39
Kelly Ng <ngkelly05@gmail.com>
2025-07-02 19:12:46 +0800

    added data, buttons slay

---
+++ aslData.js
@@ -0,0 +1,89 @@
+const aslData = {
+                "A": {
+                    sampleImage: "asl_alphabet/A1.png"
+                },
+                "B": {
+                    sampleImage: "asl_alphabet/B1.png"
+                }, 
+                "C": {
+                    sampleImage: "asl_alphabet/C1.png"
+                },
+                "D": {
+                    sampleImage: "asl_alphabet/D1.png"
+                },
+                "E": {
+                    sampleImage: "asl_alphabet/E1.png"
+                },
+                "F": {
+                    sampleImage: "asl_alphabet/F1.png"
+                },
+                "G": {
+                    sampleImage: "asl_alphabet/G1.png"
+                },
+                "H": {
+                    sampleImage: "asl_alphabet/H1.png"
+                },
+                "I": {
+                    sampleImage: "asl_alphabet/I1.png"
+                },
+                "J": {
+                    sampleImage: "asl_alphabet/J1.png"
+                },
+                "K": {
+                    sampleImage: "asl_alphabet/K1.png"
+                },
+                "L": {
+                    sampleImage: "asl_alphabet/L1.png"
+                },
+                "M": {
+                    sampleImage: "asl_alphabet/M1.png"
+                },
+                "N": {
+                    sampleImage: "asl_alphabet/N1.png"
+                },
+                "O": {
+                    sampleImage: "asl_alphabet/O1.png"
+                },
+                "P": {
+                    sampleImage: "asl_alphabet/P1.png"
+                },
+                "Q": {
+                    sampleImage: "asl_alphabet/Q1.png"
+                },
+                "R": {
+                    sampleImage: "asl_alphabet/R1.png"
+                },
+                "S": {
+                    sampleImage: "asl_alphabet/S1.png"
+                },
+                "T": {
+                    sampleImage: "asl_alphabet/T1.png"
+                },
+                "U": {
+                    sampleImage: "asl_alphabet/U1.png"
+                },
+                "V": {
+                    sampleImage: "asl_alphabet/V1.png"
+                },
+                "W": {
+                    sampleImage: "asl_alphabet/W1.png"
+                },
+                "X": {
+                    sampleImage: "asl_alphabet/X1.png"
+                },
+                "Y": {
+                    sampleImage: "asl_alphabet/Y1.png"
+                },
+                "Z": {
+                    sampleImage: "asl_alphabet/Z1.png"
+                },
+                "space": {
+                    sampleImage: "asl_alphabet/space1.png"
+                },
+                "del": {
+                    sampleImage: "asl_alphabet/del1.png"
+                }, 
+                "nothing": {
+                    sampleImage: "asl_alphabet/nothing1.png"
+                }
+            }
